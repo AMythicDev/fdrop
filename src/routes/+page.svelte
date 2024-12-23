4d406cda398b52653bc962fe88a2dea025ecd851
Arijit Dey <arijit79@protonmail.com>
2024-12-23 16:24:25 +0530

    feat: make panels in dashboard resizable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,13 @@
     type ConnectionInfo,
   } from "$lib/networking.svelte";
   import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
+  import { PaneGroup, Pane, PaneResizer } from "paneforge";
+  import { onMount } from "svelte";
+
+  let resizer: HTMLElement | undefined = $state();
+  onMount(() => {
+    resizer!.classList.add("w-1", "bg-gray-200");
+  })
 
   const webview = getCurrentWebviewWindow();
   webview.listen<string>("device-linked", (event) => {
@@ -25,24 +32,32 @@
   });
 </script>
 
-<div class="grid grid-cols-[1fr,2fr] h-[100vh]">
-  <div class="shadow-sm shadow-gray-700 flex flex-col justify-between">
-    {#each linked_devices as device}
-      <ul>
-        <li class="border-b-2 border-gray-100 px-2 py-1">{device.name}</li>
-      </ul>
-    {:else}
-      <div class="flex flex-col items-center justify-center h-full gap-2">
-        <span>No Linked Devices</span>
-        <span class="text-gray-400 text-center">
-          Click the Link Device button to link a new device
-        </span>
+<div class="h-[100vh]">
+  <PaneGroup direction="horizontal">
+    <Pane defaultSize={35} minSize={20}>
+      <div class="flex flex-col justify-between h-full">
+        {#each linked_devices as device}
+          <ul>
+            <li class="border-b-2 border-gray-100 px-2 py-1">{device.name}</li>
+          </ul>
+        {:else}
+          <div class="flex flex-col items-center justify-center h-full gap-2">
+            <span>No Linked Devices</span>
+            <span class="text-gray-400 text-center">
+              Click the Link Device button to link a new device
+            </span>
+          </div>
+        {/each}
+        <Button
+          class="!bg-blue-400 m-2"
+          onclick={() => invoke("open_link_device_window", {})}
+          >Link Device</Button
+        >
       </div>
-    {/each}
-    <Button
-      class="!bg-blue-400 m-2"
-      onclick={() => invoke("open_link_device_window", {})}>Link Device</Button
-    >
-  </div>
-  <div></div>
+    </Pane>
+    <PaneResizer bind:el={resizer} />
+    <Pane defaultSize={65} minSize={40}>
+      <div class="h-full">Hello World</div>
+    </Pane>
+  </PaneGroup>
 </div>
